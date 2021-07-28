6ebd3161414e0260cc5b466c6e5571370f8cc3ec
dmxbasil <77526924+dmxbasil@users.noreply.github.com>
2021-07-28 14:29:36 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,7 +5,7 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<<b> ഹലോ എന്റെ പേര് JONATHAN JAMES <b> <b>ഞാൻ <b><a href="https://t.me/dmx_chating">ᴅᴍx ᴄʜᴀᴛɪɴɢ</a></b> എന്ന ഗ്രൂപ്പിൽ മൂവീസ് കൊടുക്കുന്ന ബോട്ട് ആണ്
+<b> ഹലോ എന്റെ പേര് JONATHAN JAMES <b> <b>ഞാൻ <b><a href="https://t.me/dmx_chating">ᴅᴍx ᴄʜᴀᴛɪɴɢ</a></b> എന്ന ഗ്രൂപ്പിൽ മൂവീസ് കൊടുക്കുന്ന ബോട്ട് ആണ്
 എന്നെ DMX ഇൽ അല്ലാതെ വേറെ ഒരു ഗ്രൂപ്പിലും ഉപയോഗിക്കാൻ കഴിയുകയില്ല
     
     HELP_TEXT = """
